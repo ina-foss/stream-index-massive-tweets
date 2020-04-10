637489a91f14c86ce528722faecac657606d6a2e
bmaz <beatrice.mazoyer@gmail.com>
2020-04-10 12:13:30 +0200

    oauth access

--- config.py
+++ config.py
@@ -1,40 +1,29 @@
-"""
-OAuth authentification keys to Twitter API in format :
-(
-consumer_key,
-consumer_secret,
-oauth_token,
-oauth_token_secret
-)
-
-You should get your own keys to run the app. Do not try with these credentials, they are just random strings.
-"""
+# OAuth authentication keys to Twitter API
 ACCESS = [
     (
-        "mIsU1P0NNjUTf9DjuN6pdqyOF",
-        "KAd5dpgRlu0X3yizTfXTD3lZOAkF7x0QAEhAMHpVCufGW4y0t0",
-        "4087833385208874171-k6UR7OGNFdfBcqPye8ps8uBSSqOYXm",
-        "Z9nZBVFHbIsU5WQCGT7ZdcRpovQm0QEkV4n4dDofpYAEK"
+        "copy here key 0 oauth.consumerKey",
+        "copy here key 0 oauth.consumerSecret",
+        "copy here key 0 oauth.accessToken",
+        "copy here key 0 oauth.accessTokenSecret"
     ),
     (
-        "mIsU1P0NNjUTf9DjuN6pdqyOF",
-        "KAd5dpgRlu0X3yizTfXTD3lZOAkF7x0QAEhAMHpVCufGW4y0t0",
-        "4087833385208874171-k6UR7OGNFdfBcqPye8ps8uBSSqOYXm",
-        "Z9nZBVFHbIsU5WQCGT7ZdcRpovQm0QEkV4n4dDofpYAEK"
+        "copy here key 1 oauth.consumerKey",
+        "copy here key 1 oauth.consumerSecret",
+        "copy here key 1 oauth.accessToken",
+        "copy here key 1 oauth.accessTokenSecret"
     ),
     (
-        "mIsU1P0NNjUTf9DjuN6pdqyOF",
-        "KAd5dpgRlu0X3yizTfXTD3lZOAkF7x0QAEhAMHpVCufGW4y0t0",
-        "4087833385208874171-k6UR7OGNFdfBcqPye8ps8uBSSqOYXm",
-        "Z9nZBVFHbIsU5WQCGT7ZdcRpovQm0QEkV4n4dDofpYAEK"
+        "copy here key 2 oauth.consumerKey",
+        "copy here key 2 oauth.consumerSecret",
+        "copy here key 2 oauth.accessToken",
+        "copy here key 2 oauth.accessTokenSecret"
     ),
     (
-        "mIsU1P0NNjUTf9DjuN6pdqyOF",
-        "KAd5dpgRlu0X3yizTfXTD3lZOAkF7x0QAEhAMHpVCufGW4y0t0",
-        "4087833385208874171-k6UR7OGNFdfBcqPye8ps8uBSSqOYXm",
-        "Z9nZBVFHbIsU5WQCGT7ZdcRpovQm0QEkV4n4dDofpYAEK"
+        "copy here key 3 oauth.consumerKey",
+        "copy here key 3 oauth.consumerSecret",
+        "copy here key 3 oauth.accessToken",
+        "copy here key 3 oauth.accessTokenSecret"
     )
-
 ]
 
 # keywords to be tracked using the Twitter filter API and tags to indicate the collection method within the tweet body
