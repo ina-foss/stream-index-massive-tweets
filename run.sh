63e6919fa441a24d12f90f42d3045b45a356f6dd
bmaz <beatrice.mazoyer@gmail.com>
2020-04-10 17:27:17 +0200

    remove run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-docker-compose build
-docker swarm init
-mkdir -p data/es_nodes/master
-mkdir -p data/es_nodes/slave1
-mkdir -p data/es_nodes/slave2
-sudo sysctl -w vm.max_map_count=262144
-docker network create --driver overlay net0 --attachable
-docker stack deploy stream-index --compose-file docker-compose.yml --with-registry-auth
